918888ecb543dcfd9fd6e2ac156bbad651d1d915
yahia <100646468+magus59@users.noreply.github.com>
2025-02-10 08:58:44 +0100

    first commit

---
+++ src/main/java/com/yahia/diagnosticcenter/DiagnosticCenterApplication.java
@@ -0,0 +1,13 @@
+package com.yahia.diagnosticcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticCenterApplication.class, args);
+	}
+
+}
