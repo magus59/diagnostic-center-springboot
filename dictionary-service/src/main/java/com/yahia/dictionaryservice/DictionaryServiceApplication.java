1c0c53ee0282dcd8b5f44e371e5a99707347d1ea
yahia <100646468+magus59@users.noreply.github.com>
2025-02-11 15:58:35 +0100

    hghg

--- dictionary-service/src/main/java/com/yahia/dictionaryservice/DictionaryServiceApplication.java
+++ dictionary-service/src/main/java/com/yahia/dictionaryservice/DictionaryServiceApplication.java
@@ -2,8 +2,10 @@ package com.yahia.dictionaryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DictionaryServiceApplication {
 
     public static void main(String[] args) {
